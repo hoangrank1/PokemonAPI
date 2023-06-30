f201ddd4d66c85fedad8155e555e540eb39c322e
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-30 13:41:23 +0700

    Set up front and css for project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App:React.FC = () => {
   return (
     <div className="App">
-      
+      <div className="container">
+        <header className="pokemon-header">
+          Pokemon
+        </header>
+      </div>
     </div>
   );
 }
