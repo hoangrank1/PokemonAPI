f9ca7c4733f0ba74af24542b8406efb778a6512d
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-30 14:02:33 +0700

    First step to get pokemon API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
 import './App.css';
+import axios from "axios";
+
+interface Pokemons {
+  name: string;
+  url: string;
+}
+
+interface Pokemon {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+}
 
 const App:React.FC = () => {
+const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+useEffect(() => {
+  const getPokemon = async() => {
+    const res = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+    );
+    res.data.results.forEach(async (pokemon:Pokemons) => {
+      const poke = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+      );
+      setPokemons((p) => [...p, poke.data]);
+    });
+  }
+  getPokemon();
+}, []);
+
   return (
     <div className="App">
       <div className="container">
---
+++ client/src/components/PokemonColection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonColection = () => {
+  return (
+    <div>
+      PokemonColection
+    </div>
+  )
+}
+
+export default PokemonColection;
